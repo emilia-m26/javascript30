9780e83f584c06f5d30278b12d8cc678aec5aece
Emilia <emilia.mena26@gmail.com>
2021-01-06 19:02:54 -0500

    ranges update volume and playback

--- 11-custom-video-player/scripts.js
+++ 11-custom-video-player/scripts.js
@@ -28,10 +28,18 @@ function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    //console.log(this.value)
+}
+
 //hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 
-skipButtons.forEach(button => button.addEventListener('click', skip))
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
